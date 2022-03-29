545e85a55c5a68507a37d204269f63948bbbb528
Viktoria <viktoriabandura@mail.ru>
2022-03-29 15:21:04 +0300

    Add UserProfile

--- Assets/Scripts/UserProfile.cs
+++ Assets/Scripts/UserProfile.cs
@@ -1,7 +1,41 @@
+using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "UserProfile", menuName = "UserProfile")]
 public class UserProfile : ScriptableObject
 {
+    [SerializeField] 
+    private CharacteristicsSettingsProvider _characteristicsSettingsProvider;
+
+    private Dictionary<CharacteristicType, IReadOnlyReactiveProperty<int>> _characteristicsLevels;
+
+    private Wallet _wallet;
     
+
+    public void Initialize()
+    {
+        _characteristicsLevels = new ();
+        
+        //деньги будем брать из сохраненных настроек
+        _wallet = new Wallet();
+
+        var characteristicsSettings = _characteristicsSettingsProvider.GetAllCharacteristicsSettings();
+        foreach (var characteristicSetting in characteristicsSettings)
+        {
+            //уровни характеристик тут будем заполнять из сохраненных настроек
+            _characteristicsLevels[characteristicSetting.Type] = new ReactiveProperty<int>(1);
+        }
+    }
+    
+    public IReadOnlyReactiveProperty<int> GetCharacteristicLevel(CharacteristicType type)
+    {
+        return _characteristicsLevels[type];
+    }
+    
+    public void UpgradeCharacteristic(CharacteristicType type, int upgradeLevelValue, int upgradeCost)
+    {
+        ((ReactiveProperty<int>) GetCharacteristicLevel(type)).Value += upgradeLevelValue;
+        _wallet.Pay(upgradeCost);
+    }
 }
---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,17 @@
+﻿public class Wallet
+{
+    private int _money = 100;
+
+    public void Pay(int amount)
+    {
+        if (IsEnoughMoney(amount))
+        {
+            _money -= amount;
+        }
+    }
+
+    private bool IsEnoughMoney(int amount)
+    {
+        return _money >= amount;
+    }
+}
