d68b485ca4d99e4eb31ef513471d562533925dc5
Viktoria <viktoriabandura@mail.ru>
2022-03-29 20:45:52 +0300

    Add ConfigurationsProvider

---
+++ Assets/Scripts/ConfigurationsProviders/ConfigurationsProvider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ConfigurationsProviders
+{
+    [CreateAssetMenu(fileName = "ConfigurationsProvider", menuName = "ConfigurationsProvider")]
+    public class ConfigurationsProvider : ScriptableObject, IConfigurationProvider
+    {
+        public CharacteristicsSettingsProvider CharacteristicsSettingsProvider => _characteristicsSettingsProvider;
+    
+        [SerializeField] 
+        private CharacteristicsSettingsProvider _characteristicsSettingsProvider;
+  
+
+        public void Initialize()
+        {
+            _characteristicsSettingsProvider.Initialize();
+        }
+    }
+}
