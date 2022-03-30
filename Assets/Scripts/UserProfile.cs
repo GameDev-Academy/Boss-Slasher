00ad0c26d9d4dca0f3821e4730b513a00a7c5309
Viktoria <viktoriabandura@mail.ru>
2022-03-30 08:40:19 +0300

    Add AssertionExceptions to UpgradeCharacteristic in UserProfile

--- Assets/Scripts/UserProfile.cs
+++ Assets/Scripts/UserProfile.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using CharacteristicsSettings;
 using UniRx;
-using UnityEngine;
+using UnityEngine.Assertions;
 
 public class UserProfile
 {
@@ -34,6 +34,10 @@ public class UserProfile
     
     public void UpgradeCharacteristic(CharacteristicType type, int upgradeLevelValue, int upgradeCost)
     {
+        var characteristicLevel = _characteristicsLevels[type];
+        Assert.IsNotNull(characteristicLevel, $"CharacteristicLevel is null, please check the levels of {type} in " +
+                                              "characteristicsSettingsProvider");
+        
         _characteristicsLevels[type].Value += upgradeLevelValue;
         _wallet.Pay(upgradeCost);
     }
