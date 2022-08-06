48f0aa99dfa1676371c3f6d61091887057fb9a15
Vitaly S <vartovsk72@gmail.com>
2022-08-06 22:39:59 +0300

    Player refactoring

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,22 +1,52 @@
 using BattleCharacteristics;
+using Events;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-/// <summary>
-/// Player - класс медиатор, хранит ссылки на все сервисы, системы и контроллеры, завязанные на игроке,
-/// и решает зависимости между ними
-/// </summary>
-public class Player : MonoBehaviour
+namespace Player
 {
-    [FormerlySerializedAs("_movementSystem")]
-    [SerializeField] private MovementHandler _movementHandler;
-    [SerializeField] private PlayerAnimationController _animationController;
-
-
-    public void Initialize(IInputService inputService, BattleCharacteristicsManager battleCharacteristicsManager)
+    /// <summary>
+    /// Player - класс медиатор, хранит ссылки на все сервисы, системы и контроллеры, завязанные на игроке,
+    /// и решает зависимости между ними
+    /// </summary>
+    public class Player : MonoBehaviour
     {
-        var speedAsReactiveProperty = battleCharacteristicsManager.GetChatacteristic(CharacteristicType.Speed).Value;
-        _movementHandler.Initialize(inputService, speedAsReactiveProperty);
-        _animationController.Initialize(inputService);
+        [FormerlySerializedAs("_movementSystem")] [SerializeField]
+        private MovementHandler _movementHandler;
+        [SerializeField] private PlayerAnimationController _animationController;
+        [FormerlySerializedAs("_heroHealth")] [SerializeField]
+        private PlayerHealth playerHealth;
+        [SerializeField] private PlayerAttackHandler playerAttackHandler;
+
+        private bool _isDead;
+
+
+        public void Initialize(IInputService inputService, BattleCharacteristicsManager battleCharacteristicsManager)
+        {
+            var speedAsReactiveProperty =
+                battleCharacteristicsManager.GetChatacteristic(CharacteristicType.Speed).Value;
+            _movementHandler.Initialize(inputService, speedAsReactiveProperty);
+            _animationController.Initialize(inputService);
+            playerAttackHandler.Initialize(_animationController);
+
+            SetMaxHealth(battleCharacteristicsManager);
+            playerHealth.IsDead.SubscribeWhenTrue(OnPlayerDied);
+        }
+
+        private void SetMaxHealth(BattleCharacteristicsManager battleCharacteristicsManager)
+        {
+            playerHealth.InitializePlayerHealth(
+                battleCharacteristicsManager.GetChatacteristic(CharacteristicType.Health).Value.Value);
+        }
+
+        private void OnPlayerDied()
+        {
+            if (!_isDead)
+            {
+                _isDead = true;
+                _animationController.PlayDeath();
+                EventStreams.UserInterface.Publish(new DungeonPassEvent(false));
+            }
+        }
     }
 }
